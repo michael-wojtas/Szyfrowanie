cb6d97926e70284770960bb8ba716bdbd797c17f
michael-wojtas <mailto:mwojtas7@zstigliwicepl.onmicrosoft.com>
2025-06-03 19:27:45 +0200

    hash

---
+++ hashowanie.py
@@ -0,0 +1,14 @@
+import hashlib
+
+
+dane1 = "Ala ma kota"
+dane2 = "Ala ma psa"
+
+
+md5 = hashlib.md5(dane1.encode()).hexdigest()
+
+sha256 = hashlib.sha256(dane1.encode()).hexdigest()
+
+
+print("MD5 (Ala ma kota):   ", md5)
+print("SHA-256 (Ala ma kota):", sha256)
